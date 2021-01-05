0b8d3ccafd7de70cbdcc8e4cd1bcb7bf71850827
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-01-05 14:33:45 -0600

    Created Components
    Added Components
    
    Added Component Styles
    
    Bootstrap NavBar
    
    feat(o-shop): Removed placeholder comment, console statement, updated stylesheet
    
    feat(o-shop): Additional style update
    
    feat(o-shop): removed colors.scss, useEffect from NavBar, added index.js newline

---
+++ apprentice-react-o-shop/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss'
+
+function Header() {
+    return (
+        <header className="container-fluid">
+            <h1>Our React Starter</h1>
+        </header>
+    )
+}
+
+export default Header;
---
+++ apprentice-react-o-shop/src/components/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Main.scss'
+
+function Main() {
+    return (
+        <main>
+            <h2>This is the Main Component</h2>
+        </main>
+    )
+}
+
+export default Main;
