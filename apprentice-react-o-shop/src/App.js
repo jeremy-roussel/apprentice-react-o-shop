087a4c9465595b3424c66e715a3eaefabce8d4ef
Jeremy Roussel <jeremy.roussel@umbrage.com>
2021-01-11 09:45:02 -0600

    feat(o-shop): add route protection to main links

--- apprentice-react-o-shop/src/App.js
+++ apprentice-react-o-shop/src/App.js
@@ -1,13 +1,24 @@
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
-import NavBar from './components/NavBar/NavBar';
+import {Route, Switch} from 'react-router';
+
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import PrivateRoute from './components/Shared/PrivateRoute'
+// import Header from './components/Header/Header';
+// import Main from './components/Main/Main';
+// import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="register" component={Register} />
+        <PrivateRoute path="*" component={Home} />
+      </Switch>
+      {/* <NavBar/>
       <Header/>
-      <Main/>
+      <Main/> */}
     </div>
   );
 }
--- apprentice-react-o-shop/src/pages/Home.js
+++ apprentice-react-o-shop/src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import Header from '../components/Header/Header';
+import NavBar from '../components/NavBar/NavBar';
+import Main from '../components/Main/Main';
 
 function Home() {
   return (
-    <h1>Home Works!</h1>
+    <main>
+      <NavBar />
+      <Header />
+      <Main />
+    </main>
   );
 }
 
